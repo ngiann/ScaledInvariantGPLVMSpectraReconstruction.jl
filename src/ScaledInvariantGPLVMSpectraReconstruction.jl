87eafd70c22284d346211193df2edf3552032c5b
Nikos Gianniotis <ngiann@gmail.com>
2025-05-12 17:53:03 +0200

    remove commented out lines

--- src/ScaledInvariantGPLVMSpectraReconstruction.jl
+++ src/ScaledInvariantGPLVMSpectraReconstruction.jl
@@ -17,11 +17,10 @@ module ScaledInvariantGPLVMSpectraReconstruction
     using ForwardNeuralNetworks
 
     include("AbstractBackend.jl")
-    # include("loadmnist.jl")
+
     include("initialmlscaleinvariantgplvmsolution.jl")
     include("calculatecovariance.jl")
-    # include("gp.jl")
-    # include("rectifiergp.jl")
+
     include("util/verify_bound_mlscaleinvariantgplvm.jl")
     include("util/verify_bound_gplvm.jl")
     include("util/woodbury.jl")
@@ -36,11 +35,7 @@ module ScaledInvariantGPLVMSpectraReconstruction
     include("mlscaleinvariantgplvm.jl")
     include("numberofparameters_mlscaleinvariantgplvm.jl")
     include("mllowerbound.jl")
-    # include("grad/lowerbound_for_AD.jl")
-    # include("grad/helper_grad_manual.jl")
-    # include("grad/helper_grad_ad.jl")
-    # include("grad/helper_grad.jl")
-
+    
     include("lowerbound_gplvm.jl")
     include("gplvm.jl")
     include("initialgplvmsolution.jl")
